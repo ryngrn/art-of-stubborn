e6999083577377b6dd5c9d05822856c7e2b8da90
Ryan Green <ryan@Ryans-MBP.hsd1.tn.comcast.net>
2022-02-02 16:15:14 -0600

    coming soon

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        //<Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <img src="/stubborn-mule.png" class="coming-soon-image" />
         </p>
       </main>
 
